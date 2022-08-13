c6b7c5bd52c18312870fd2fd6d6f6e5fdf0675ee
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-08-14 01:40:29 +0530

    Add files via upload

---
+++ Array1.py
@@ -0,0 +1,14 @@
+from array import *
+vals1= array('i', [3,4,5,2,15,78])
+vals2= array('i', [3,4,-5,2,-15,78])  #Signed integer
+Ivals= array('I', [2,1,3])  #Unsigned integer
+Fvals= array('f', [3.65,5.87,13,8,9,34.91])
+print(vals1)
+print(vals2)
+print(Ivals)
+print(Fvals)
+vals= array('i', [3,4,5,2,15,78])
+vals.reverse()
+print(vals)
+print(vals2[0])
+print(vals2[4])
