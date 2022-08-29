c96f638d708407acf2dd20826b4ee62b8af0eda0
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-08-30 01:41:17 +0530

    Update Array9Sum.py

--- Array9Sum.py
+++ Array9Sum.py
@@ -1,6 +1,6 @@
 from array import *
 arr= array('i',[2,4,3,5,6])
-newarr= array(arr.typecode, (a+sum for a in arr))
+newarr= array(arr.typecode, (a+a for a in arr))
 i=0
 while(i<len(arr)):
     print(newarr[i],end=" ")
